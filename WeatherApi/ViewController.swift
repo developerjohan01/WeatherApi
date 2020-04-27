9bda6921d4d6e9972bdbab07ab8c36ef19b89868
Johan Nilsson <developerjohan01@gmail.com>
2020-04-27 12:20:51 +0200

    Second commit - the new starter project

---
+++ WeatherApi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApi
+//
+//  Created by Johan Nilsson on 2020/04/27.
+//  Copyright © 2020 Johan Nilsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
